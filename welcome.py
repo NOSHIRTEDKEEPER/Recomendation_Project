250bc8aa40ebfdb6c37fe574144856f06b0f0901
Harry Holt <117466542+NOSHIRTEDKEEPER@users.noreply.github.com>
2023-01-26 20:40:01 -0500

    Add files via upload

---
+++ welcome.py
@@ -0,0 +1,5 @@
+def print_welcome():
+    print("________________________________")
+    print("*  Welcome to The Restaurant   *")
+    print("*           Search.            *")
+    print("*______________________________*")
